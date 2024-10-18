9a5de3c3582a8fe01939714a3f88e22aae5c3079
Halva1988 <khalaimaleksandr@gmail.com>
2024-10-18 10:28:18 +0300

    создал компоненты: BackgroundHeadin, Footer, Header, ItemList, Sidebar, добавил стили

---
+++ src/components/Footer.jsx
@@ -0,0 +1,12 @@
+
+
+const Footer = () => {
+  return (
+    <footer>
+      <small>&copy; The copyright belongs to Halva. All rights reserved.</small>
+      <p>Version <b>0.1</b></p>
+    </footer>
+  )
+}
+
+export default Footer
